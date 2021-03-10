c05d683343de0ff621519a76de9e4faf567f1613
luojie2020 <luojie@yeezon.com>
2021-03-10 16:20:34 +0800

    add readme

--- lib/yol_qy_weixin/client.rb
+++ lib/yol_qy_weixin/client.rb
@@ -18,12 +18,3 @@ module YolQyWeixin
     end
   end
 end
-
-# gem uninstall yol_qy_weixin
-# gem build yol_qy_weixin     
-# gem install ./yol_qy_weixin-0.0.1.gem
-# require "yol_qy_weixin"
-# qy_weixin = YolQyWeixin::Client.new(corpid: "wx6fcd91cc5993e305", secret: "-eE2GYJlqK1npg4_x6lj2PXIFD7gmLz7stqE6CCyj3s", redis: "")
-# require 'net/http'
-# require 'json'
-# qy_weixin.get_user_info(333)
--- lib/yol_qy_weixin/version.rb
+++ lib/yol_qy_weixin/version.rb
@@ -1,3 +1,3 @@
 module YolQyWeixin
-  VERSION = "0.0.1"
+  VERSION = "0.0.3"
 end
